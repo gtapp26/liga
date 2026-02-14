9b925f3f19a38909e5b32c71dff3b903b3b658f9
gtapp26 <olivervl2000@gmail.com>
2026-02-14 13:03:22 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10610861
+    "domain": "3nbf4.com",
+    "zoneId": 10610758
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
